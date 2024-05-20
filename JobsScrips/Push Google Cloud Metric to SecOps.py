701c9ecac65d72b38b4061ebbf985f7f9f643184
pilot006 <pilotalb@gmail.com>
2024-05-20 09:11:30 -0400

    1.1 Update
    Update to support multiple metrics

--- JobsScrips/Push Google Cloud Metric to SecOps.py
+++ JobsScrips/Push Google Cloud Metric to SecOps.py
@@ -18,46 +18,21 @@ gcp_metrics_sa = json.loads(gcp_metrics_sa)
 secops_sa = siemplify.extract_job_param(param_name="SecOps Ingestion API JSON", print_value=False)
 secops_sa = json.loads(secops_sa)
 project = siemplify.extract_job_param(param_name="GCP Project Name", print_value=True)
-metric = siemplify.extract_job_param(param_name="Metric", print_value=True)
+metric = siemplify.extract_job_param(param_name="Metric(s)", print_value=True)
 customer_id = siemplify.extract_job_param(param_name="SecOps Tenant ID", print_value=True)
 
 
 def main():
 
     try:
-        # Current time
-        now = datetime.utcnow()
-
-        # Set the start time to 4 minutes from now. This should provide us the most recent metrics
-        start_time = now - timedelta(minutes=4)
-
-        # Define the filter for the requested metric
-        filter_ = (
-            metric
-        )
-
-         # URL for Monitoring API endpoint
-        url = f"https://monitoring.googleapis.com/v3/projects/{project}/timeSeries?filter={filter_}&interval.startTime={start_time.isoformat()+'Z'}&interval.endTime={now.isoformat()+'Z'}&view=FULL"
-
-        credentials = service_account.Credentials.from_service_account_info(
-            gcp_metrics_sa, scopes=['https://www.googleapis.com/auth/cloud-platform']
-        )
-        request = google.auth.transport.requests.Request()
-        credentials.refresh(request)
-        hdrs = {
-            "Authorization": "Bearer " + credentials.token,
-            "Content-Type": "application/json"
-             }
-        response = requests.get(url, headers=hdrs)
-        #siemplify.LOGGER.info("Metrics API response:" + response.text)
-
-        if response.status_code == 200:
-            data = json.loads(response.text)
-            for time_series in data.get("timeSeries", []):
-                siemplify.LOGGER.info(json.dumps(time_series, indent=1))
-                send_to_chronicle(time_series)
+        if ',' in metric:
+            siemplify.LOGGER.info('Multiple metrics detected in: ' + metric)
+            m_arr = metric.split(',')
+            for m in m_arr:
+                get_metric(m)
         else:
-            print(f"Error: {response.status_code} - {response.text}")
+            siemplify.LOGGER.info('Single metric detected in: ' + metric)
+            get_metric(metric)
 
     except Exception as e:
         siemplify.LOGGER.error("General error performing job {}".format(SCRIPT_NAME))
@@ -65,6 +40,41 @@ def main():
         raise
 
     siemplify.end_script()
+
+def get_metric(metric):
+    # Current time
+    now = datetime.utcnow()
+
+    # Set the start time to 4 minutes from now. This should provide us the most recent metrics
+    start_time = now - timedelta(minutes=4)
+
+    # Define the filter for the requested metric
+    filter_ = (
+        metric
+    )
+
+    # URL for Monitoring API endpoint
+    url = f"https://monitoring.googleapis.com/v3/projects/{project}/timeSeries?filter={filter_}&interval.startTime={start_time.isoformat()+'Z'}&interval.endTime={now.isoformat()+'Z'}&view=FULL"
+
+    credentials = service_account.Credentials.from_service_account_info(
+        gcp_metrics_sa, scopes=['https://www.googleapis.com/auth/cloud-platform']
+    )
+    request = google.auth.transport.requests.Request()
+    credentials.refresh(request)
+    hdrs = {
+        "Authorization": "Bearer " + credentials.token,
+        "Content-Type": "application/json"
+    }
+    response = requests.get(url, headers=hdrs)
+    siemplify.LOGGER.info("Metrics API response:" + response.text)
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        for time_series in data.get("timeSeries", []):
+            siemplify.LOGGER.info(json.dumps(time_series, indent=1))
+            send_to_chronicle(time_series)
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
 
 def send_to_chronicle(log_line):
     raw_event = {
